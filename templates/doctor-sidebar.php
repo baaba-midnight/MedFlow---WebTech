7cc0bf1065d985270b7ba3f30e1896e11efe86b5
Jonathan-KAB <jonathan.boateng@ashesi.edu.gh>
2024-11-07 00:01:51 +0000

    fixed doctor dashboard to a greater extent, rushing to work on other views before modals. Apologies for the delay

--- templates/doctor-sidebar.php
+++ templates/doctor-sidebar.php
@@ -1,14 +1,14 @@
 <!-- Sidebar -->
 <div class="sidebar">
     <div class="logo-section">
-        <img src="../../assets/images/medflow-mini-logo.png" alt="MedFlow">
+        <img src="../../assets/images/medflow_mini_logo.png" alt="MedFlow">
         <h1>MedFlow</h1>
     </div>
     
     <nav class="nav flex-column mt-4">
         <a class="nav-link active" href="#"><i class="fas fa-home me-2"></i> Dashboard</a>
         <a class="nav-link" href="#"><i class="fas fa-user-injured me-2"></i> Patient Management</a>
-        <a class="nav-link" href="#"><i class="fas fa-file-invoice-dollar me-2"></i> Billing</a>
+        <!-- <a class="nav-link" href="#"><i class="fas fa-file-invoice-dollar me-2"></i> Billing</a> -->
     </nav>
 
     <div class="user-profile">
