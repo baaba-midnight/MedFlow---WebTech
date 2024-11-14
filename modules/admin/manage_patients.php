2639e31eda6ac49ffeb6dc5bea17851ad9b99493
Baaba Onomaa Amosah <146720493+baaba-midnight@users.noreply.github.com>
2024-11-14 14:19:05 +0000

    adding two lines to manage_patient

--- modules/admin/manage_patients.php
+++ modules/admin/manage_patients.php
@@ -145,6 +145,7 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../../assets/js/edit_patient_modal.js"></script>
+    <script src="../../assets/js/fetch_patients.js"></script>
+    <script src="../../assets/js/edit-patient.js"></script>
 </body>
 </html>
